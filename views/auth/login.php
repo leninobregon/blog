70da9087757dcc7629c913feec2c86c4c93566c0
leninobregon <109239188+leninobregon@users.noreply.github.com>
2026-04-13 08:22:16 -0600

    Fix baseUrl in auth/login and scan for errors

--- views/auth/login.php
+++ views/auth/login.php
@@ -11,6 +11,11 @@ $currentTheme = getActiveTheme();
 $colors = getThemeColors($currentTheme);
 $config = require __DIR__ . '/../../config.php';
 
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+$host = $_SERVER['HTTP_HOST'] ?? 'localhost';
+$scriptDir = dirname($_SERVER['SCRIPT_NAME']);
+$baseUrl = $protocol . '://' . $host . $scriptDir;
+
 $error = $error ?? '';
 $success = $success ?? '';
 $isLogin = $isLogin ?? true;
