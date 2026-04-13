c1ffd45d4d94efb8728950d17199b0dfcdba3a7f
leninobregon <109239188+leninobregon@users.noreply.github.com>
2026-04-13 08:14:08 -0600

    Fix user_id check syntax

--- user/index.php
+++ user/index.php
@@ -3,7 +3,10 @@ if(session_status() === PHP_SESSION_NONE) session_start();
 header('Cache-Control: no-cache, no-store, must-revalidate');
 header('Pragma: no-cache');
 header('Expires: 0);
-if(empty($_SESSION['user_id'])) { header('Location: ../auth.php'); exit; }
+if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
+    header('Location: ../auth.php');
+    exit;
+}
 include '../includes/functions.php';
 
 // Language
